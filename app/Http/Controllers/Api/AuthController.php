ec329fa742db3da8c6e6b440d5d4ca1c2a451caf
susmoysb <susmoysb@gmail.com>
2025-02-22 04:16:29 +0600

    login API done

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -9,6 +9,7 @@ use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 
@@ -40,11 +41,11 @@ class AuthController extends Controller
     public function register(Request $request): JsonResponse
     {
         $validator = Validator::make($request->all(), [
-            'name' => ['required', 'string', 'min:2', 'max:255'],
-            'username' => ['required', 'string', 'min:2', 'max:30', Rule::unique('users')],
+            'name'        => ['required', 'string', 'min:2', 'max:255'],
+            'username'    => ['required', 'string', 'min:2', 'max:30', Rule::unique('users')],
             'employee_id' => ['required', 'string', 'min:2', 'max:30', Rule::unique('users')],
-            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users')],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'email'       => ['required', 'string', 'email', 'max:255', Rule::unique('users')],
+            'password'    => ['required', 'string', 'min:8', 'confirmed'],
         ]);
 
         $validatedData = $validator->validated();
@@ -60,8 +61,8 @@ class AuthController extends Controller
             return self::withCreated(
                 'User ' . self::MESSAGES['register'],
                 [
-                    'user' => $user,
-                    'token' => $token,
+                    'user'       => $user,
+                    'token'      => $token,
                     'token_type' => 'Bearer'
                 ]
             );
@@ -69,5 +70,53 @@ class AuthController extends Controller
             DB::rollBack();
             return self::withBadRequest(self::MESSAGES['system_error'], $e->getMessage() . ' ' . get_class($e));
         }
+    }
+
+    /**
+     * Handle the login request.
+     *
+     * This method validates the login credentials provided in the request.
+     * It accepts a 'login' field which can be a username, employee_id, or email, and a 'password' field
+     * If the credentials are valid, it generates a new personal access token for the user with specified abilities and returns it along with user data.
+     * If the credentials are invalid, it returns an unauthorized response.
+     *
+     * @param \Illuminate\Http\Request $request The incoming request instance.
+     *
+     * @return \Illuminate\Http\JsonResponse The response containing user data and access token, or an unauthorized response.
+     *
+     * @throws \Illuminate\Validation\ValidationException If the validation fails.
+     */
+    public function login(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'login'    => ['required', 'string'],   // Can be username, employee_id, or email
+            'password' => ['required', 'string'],
+        ]);
+
+        $validatedData = $validator->validated();
+
+        $login    = $validatedData['login'];
+        $password = $validatedData['password'];
+
+        $user = User::where('username', $login)
+            ->orWhere('employee_id', $login)
+            ->orWhere('email', $login)
+            ->first();
+
+        if ($user && Hash::check($password, $user->password)) {
+            // Create a new personal access token for the user
+            $token = $this->personalAccessTokenService->store($request, $user);
+
+            return $token ? self::withOk(
+                'User ' . self::MESSAGES['login'],
+                [
+                    'user'       => $user,
+                    'token'      => $token,
+                    'token_type' => 'Bearer'
+                ]
+            ) : self::withInternalServerError(self::MESSAGES['system_error']);
+        }
+
+        return self::withUnauthorized(self::MESSAGES['invalid_credentials']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('register', 'register');
+    Route::post('login', 'login');
 });
 
 Route::get('/user', function (Request $request) {
